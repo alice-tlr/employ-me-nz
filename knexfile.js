f68ca3d1f79691bbd3c9aba8b7973b9907b4740c
Alice Rosier <alicetlrosier@gmail.com>
2016-12-22 13:07:03 +1300

    updated knexfile and package.json for deployment

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,14 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    useNullAsDefault: true
+  }
+
   staging: {
     client: 'postgresql',
     connection: {
@@ -28,11 +36,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   database: 'my_db',
+    //   user:     'username',
+    //   password: 'password'
+    // },
     pool: {
       min: 2,
       max: 10
